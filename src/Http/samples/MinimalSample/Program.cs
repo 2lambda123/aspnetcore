3c2a9df04d883ef2c78755378a08cbef6528447b
Stephen Halter <halter73@gmail.com>
2022-10-12 11:40:49 -0700

    Reintroduce filters

--- src/Http/samples/MinimalSample/Program.cs
+++ src/Http/samples/MinimalSample/Program.cs
@@ -1,15 +1,53 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Reflection;
+using System.Text;
 using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 app.MapGet("/", (EndpointDataSource dataSource)
     => EndpointDataSource.GetDebuggerDisplayStringForEndpoints(dataSource.Endpoints));
+
+app.MapGet("/hello/{name}", (string name) => $"Hello {name}!")
+    .AddEndpointFilterFactory((context, next) =>
+    {
+        var parameters = context.MethodInfo.GetParameters();
+        // Only operate handlers with a single argument
+        if (parameters.Length == 1 &&
+            parameters[0] is ParameterInfo parameter &&
+            parameter.ParameterType == typeof(string))
+        {
+            return invocationContext =>
+            {
+                var modifiedArgument = invocationContext
+                    .GetArgument<string>(0)
+                    .ToUpperInvariant();
+                invocationContext.Arguments[0] = modifiedArgument;
+                return next(invocationContext);
+            };
+        }
+
+        return invocationContext => next(invocationContext);
+    });
+
+app.MapControllers()
+    .AddEndpointFilter((invocationContext, next) =>
+    {
+        var argument = invocationContext.GetArgument<string>(0);
+        if (argument != null)
+        {
+            invocationContext.Arguments[0] = Convert.ToBase64String(Encoding.UTF8.GetBytes(argument));
+        }
+        return next(invocationContext);
+    });
 
 ((IEndpointRouteBuilder)app).DataSources.Add(new CustomEndpointDataSource());
 
@@ -17,7 +55,7 @@ app.Run();
 
 class CustomEndpointDataSource : EndpointDataSource
 {
-    public override IReadOnlyList<Endpoint> Endpoints => Enumerable.Range(0, 10).Select(CreateEndpoint).ToArray();
+    public override IReadOnlyList<Endpoint> Endpoints => new[] { CreateEndpoint(0), CreateEndpoint(1) };
 
     public override IChangeToken GetChangeToken() => NullChangeToken.Singleton;
 
