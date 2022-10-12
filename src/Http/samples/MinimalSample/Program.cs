a6d1feca77da28dbd3ee4b45074a934e1e8476a4
Stephen Halter <halter73@gmail.com>
2022-10-12 11:30:29 -0700

    Add CustomEndpointDataSource

--- src/Http/samples/MinimalSample/Program.cs
+++ src/Http/samples/MinimalSample/Program.cs
@@ -1,9 +1,35 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.AspNetCore.Routing.Patterns;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (EndpointDataSource dataSource)
+    => EndpointDataSource.GetDebuggerDisplayStringForEndpoints(dataSource.Endpoints));
+
+((IEndpointRouteBuilder)app).DataSources.Add(new CustomEndpointDataSource());
 
 app.Run();
+
+class CustomEndpointDataSource : EndpointDataSource
+{
+    public override IReadOnlyList<Endpoint> Endpoints => Enumerable.Range(0, 10).Select(CreateEndpoint).ToArray();
+
+    public override IChangeToken GetChangeToken() => NullChangeToken.Singleton;
+
+    static Endpoint CreateEndpoint(int id)
+    {
+        var displayName = $"Custom endpoint #{id}";
+        var metadata = new EndpointMetadataCollection(new[] { new RouteNameMetadata(displayName) });
+
+        return new RouteEndpoint(
+            context => context.Response.WriteAsync(displayName),
+            RoutePatternFactory.Parse($"/custom/{id}"),
+            order: 0, metadata, displayName);
+    }
+}
+
